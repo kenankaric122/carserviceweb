c2cbed95083db0db3da91492cf144c54e566a2d4
kenankaric122 <karickenan97@gmail.com>
2022-01-08 00:13:36 +0100

    :art: improved site

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles/Home.css';
+
+function Home() {
+    return (
+        <div className='home'>
+            <div className='hometxt'>
+                <h1>Car Repair Service</h1>
+                <h3>Place where you can repair your vehicles.</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
