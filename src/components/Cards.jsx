8bb36c7d8928feac5ce95bf1fb9209d8483678db
kenankaric122 <karickenan97@gmail.com>
2022-01-08 00:45:00 +0100

    :sparkles: new features

---
+++ src/components/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Cards.css';
+
+function Cards() {
+    return (
+        <div className='cards'>
+            asdasdsad
+        </div>
+    )
+}
+
+export default Cards;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Home from '../components/Home'
 import More from '../components/More'
+import Cards from '../components/Cards'
 
 const Landing = () => {
     return (
         <div>
             <Navbar/>
             <Home/>
+            <Cards/>
             <More/>
         </div>
     )
