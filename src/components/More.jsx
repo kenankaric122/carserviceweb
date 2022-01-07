e797a3f98d173a8e9ea7a2fa37e1e61e6cbdedef
kenankaric122 <karickenan97@gmail.com>
2022-01-08 00:42:07 +0100

    :sparkles: new features

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -5,10 +5,10 @@ import image from '../assets/supra.jpg'
 function More() {
     return (
         <div className='more'>
-            <div className='img1' style={{ backgroundImage: `url(${image})` }}>
-            </div>
+            <div className='img1' style={{ backgroundImage: `url(${image})` }}></div>
             <div className='text1'>
-                tex1
+            A car service is a health check with routine maintenance for your vehicle which assesses everything from your engine’s fluid levels to the general wear and tear of your car. 
+            A service is usually undertaken by a mechanic and the most in-depth of them involve around 50 or more system and component checks, adjustments and parts replacement.
             </div>
         </div>
     )
