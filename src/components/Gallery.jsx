780679d5ce07c1f462cd746098fbd770949f5743
kenankaric122 <karickenan97@gmail.com>
2022-01-08 19:50:47 +0100

    :sparkles: new features

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/Gallery.css'
+
+function Gallery() {
+    return (
+        <div className='gal'>
+        <h1 style={{color : "#404040", textAlign : "center", marginTop : "15%", fontWeight : "bold"}}><br/><br/><span>/ </span>Gallery<span> /</span></h1>
+        <div className='gallery'>
+            asdasd
+        </div>
+        </div>
+    )
+}
+
+export default Gallery
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Home from '../components/Home'
 import More from '../components/More'
 import Cards from '../components/Cards'
+import Gallery from '../components/Gallery'
 
 const Landing = () => {
     return (
@@ -11,6 +12,7 @@ const Landing = () => {
             <Home/>
             <Cards/>
             <More/>
+            <Gallery/>
         </div>
     )
 }
