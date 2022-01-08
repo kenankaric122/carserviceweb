7e2d4aaa3eae75c567dcc9136b576895be3ccdd5
kenankaric122 <karickenan97@gmail.com>
2022-01-08 21:46:58 +0100

    :sparkles: new navigation features

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter} from 'react-router-dom'
-import Landing from './pages/Landing'
+import Landing from './pages/Landing';
+import Reservations from './pages/Reservations';
+import Gallery from './pages/Gallery';
+import Aboutus from './pages/Aboutus';
 
 const Navigation = () => {
     return (
         <BrowserRouter>
             <Switch>
             <Route path="/" exact component={Landing}/>
+            <Route path="/reservations" exact component={Reservations}/>
+            <Route path="/gallery" exact component={Gallery}/>
+            <Route path="/aboutus" exact component={Aboutus}/>
             </Switch>
         </BrowserRouter>
     )
