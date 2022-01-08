0ee703d1c4bb1e3b45c98dd401925974fd9f154b
kenankaric122 <karickenan97@gmail.com>
2022-01-08 20:17:21 +0100

    :sparkles: new navbar features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useState } from 'react';
 import './styles/Navbar.css';
 import logo from '../assets/logo.svg'
 
 const Navbar = () => {
+    
+    const[navbar, setNavbar] = useState(false);
+
+    const changeBg = () => {
+        if(window.scrollY >= 900){
+            setNavbar(true);
+        }
+        else{
+            setNavbar(false);
+        }
+    }
+
+    window.addEventListener('scroll', changeBg);
+
     return (
-        <nav>
+        <nav className={navbar ? 'navbar active' : 'navbar'}>
             <img className='logo' src={logo} alt='logo'/>
             <ul>
                 <li>Home</li>
