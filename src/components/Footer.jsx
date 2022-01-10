dcaea8ca123fb17d909297ef1e0ad270dbf0fe19
kenankaric122 <karickenan97@gmail.com>
2022-01-10 20:36:38 +0100

    :sparkles: new footer features

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,22 @@ import './styles/Footer.css';
 function Footer() {
     return (
         <div className='footer'>
-            asd
+            <div className='f1'>
+                <h1>Information</h1>
+                <p>ftarasrf</p>
+            </div>
+            <div className='f1'>
+                <h1>Quicklink</h1>
+                <p>transff</p>
+            </div>
+            <div className='f1'>
+                <h1>Support</h1>
+                <p>transff</p>
+            </div>
+            <div className='f1'>
+                <h1>Store information</h1>
+                <p>transff</p>
+            </div>
         </div>
     )
 }
