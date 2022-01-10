fec68b8c187af2c8a61797cf6cf0d50ac2b86700
kenankaric122 <karickenan97@gmail.com>
2022-01-10 21:17:22 +0100

    :lipstick: footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { faAddressBook, faAddressCard, faMailBulk, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles/Footer.css';
@@ -15,24 +17,34 @@ function Footer() {
                 <button>Our services</button><br/>
                 <button>Careers</button>
             </div>
+
             <div className='f1'>
             <br/><br/>
-                <h1>Quicklink</h1>
+                <h1>Quicklink</h1><br/>
                 <button><Link to = '/' className='link'>Home</Link></button><br/>
                 <button><Link to = '/reservations' className='link'>Reservations</Link></button><br/>
                 <button><Link to = '/gallery' className='link'>Gallery</Link></button><br/>
                 <button><Link to = '/aboutus' className='link'>About us</Link></button><br/>
             </div>
+
             <div className='f1'>
             <br/><br/>
-                <h1>Support</h1>
-                <p>transff</p>
+                <h1>Support</h1><br/>
+                <button><Link to = '/' className='link'>Blog</Link></button><br/>
+                <button><Link to = '/reservations' className='link'>Contact</Link></button><br/>
+                <button><Link to = '/' className='link'>Online support</Link></button><br/>
+                <button><Link to = '/' className='link'>Money back</Link></button><br/>
             </div>
+
             <div className='f1'>
             <br/><br/>
-                <h1>Store information</h1>
-                <p>transff</p>
+                <h1>Informations</h1><br/>
+            <FontAwesomeIcon icon={faAddressCard} className='icons'/>  2222 New York, Central Park <br/>
+            <FontAwesomeIcon icon={faAddressCard} className='icons'/>  1200 USA  <br/>
+            <FontAwesomeIcon icon={faMailBulk} className='icons'/>  https://gmail.com  <br/>
+            <FontAwesomeIcon icon={faPhone} className='icons'/>  +85 8854 7875  <br/>
             </div>
+
         </div>
     )
 }
