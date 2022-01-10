800b354bb0bdee03c9fde67a6ba4353d91024ebe
kenankaric122 <karickenan97@gmail.com>
2022-01-10 20:57:04 +0100

    :sparkles: new footer features

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles/Footer.css';
 
 
 function Footer() {
     return (
         <div className='footer'>
+
             <div className='f1'>
-                <h1>Information</h1>
-                <p>ftarasrf</p>
+            <br/><br/>
+                <h1>Information</h1><br/>
+                <button><Link to = '/aboutus' className='link'>Our company</Link></button><br/>
+                <button><Link to = '/reservations' className='link'>Contact us</Link></button><br/>
+                <button>Our services</button><br/>
+                <button>Careers</button>
             </div>
             <div className='f1'>
+            <br/><br/>
                 <h1>Quicklink</h1>
-                <p>transff</p>
+                <button><Link to = '/' className='link'>Home</Link></button><br/>
+                <button><Link to = '/reservations' className='link'>Reservations</Link></button><br/>
+                <button><Link to = '/gallery' className='link'>Gallery</Link></button><br/>
+                <button><Link to = '/aboutus' className='link'>About us</Link></button><br/>
             </div>
             <div className='f1'>
+            <br/><br/>
                 <h1>Support</h1>
                 <p>transff</p>
             </div>
             <div className='f1'>
+            <br/><br/>
                 <h1>Store information</h1>
                 <p>transff</p>
             </div>
