323eed188ab1ebd7a670549c5962691519258a7c
kenankaric122 <karickenan97@gmail.com>
2022-01-10 14:59:52 +0100

    :lipstick: landing page design updated

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import './styles/Choose.css'
 
 function Choose() {
     return (
         <div className='chs2'>
-        <h1 style={{color : "#404040", textAlign : "center", marginTop : "10%", fontWeight : "bold"}}><span>/ </span>Why choose us?<span> /</span></h1>
+        <h1 style={{color : "#404040", textAlign : "center", marginTop : "10%", fontWeight : "bold", backgroundColor : "rgb(230, 230, 230)"}}><br/><br/><span>/ </span>Why choose us?<span> /</span></h1>
         <div className='choose'>
+            <FontAwesomeIcon icon = {faHeart} size = '3x'></FontAwesomeIcon>
         </div>
         </div>
     )
