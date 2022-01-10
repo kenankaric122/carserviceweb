02b0377d15e98219b4fb5bfd8e73e8dcf0db70a0
kenankaric122 <karickenan97@gmail.com>
2022-01-10 15:15:39 +0100

    :lipstick: landing page updated

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChair, faHeart, faIdBadge } from '@fortawesome/free-solid-svg-icons'
+import { faCertificate, faChair, faHeart, faIdBadge, faOilCan, faPeopleArrows } from '@fortawesome/free-solid-svg-icons'
 import './styles/Choose.css'
 
 function Choose() {
@@ -10,25 +10,25 @@ function Choose() {
         <p style={{color : "404040", textAlign : "center", marginLeft : "10%", marginRight : "10%"}}>We are a family-owned business that has been serving the local community since 1990. We always strive for the highest quality, honest and friendly service, and do our best to help you as quickly as possible.</p>
         <div className='choose'>
             <div className='icons'>
-            <FontAwesomeIcon icon = {faHeart} size = '3x'></FontAwesomeIcon>
+            <FontAwesomeIcon icon = {faHeart} size = '3x' color='#ba382f'></FontAwesomeIcon>
             </div>
             <div className='icons'>
-            <FontAwesomeIcon icon = {faIdBadge} size = '3x'></FontAwesomeIcon>
+            <FontAwesomeIcon icon = {faIdBadge} size = '3x' color='#ba382f'></FontAwesomeIcon>
             </div>
             <div className='icons'>
-            <FontAwesomeIcon icon = {faChair} size = '3x'></FontAwesomeIcon>
+            <FontAwesomeIcon icon = {faChair} size = '3x' color='#ba382f'></FontAwesomeIcon>
             </div>
         </div>
 
         <div className='choose1'>
             <div className='icons'>
-            <FontAwesomeIcon icon = {faHeart} size = '3x'></FontAwesomeIcon>
+            <FontAwesomeIcon icon = {faCertificate} size = '3x' color='#ba382f'></FontAwesomeIcon>
             </div>
             <div className='icons'>
-            <FontAwesomeIcon icon = {faIdBadge} size = '3x'></FontAwesomeIcon>
+            <FontAwesomeIcon icon = {faOilCan} size = '3x' color='#ba382f'></FontAwesomeIcon>
             </div>
             <div className='icons'>
-            <FontAwesomeIcon icon = {faChair} size = '3x'></FontAwesomeIcon>
+            <FontAwesomeIcon icon = {faPeopleArrows} size = '3x' color='#ba382f'></FontAwesomeIcon>
         </div>
 
 
