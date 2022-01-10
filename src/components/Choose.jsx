87098ab8374a7b0a1a639ef641b86f11b61c81a9
kenankaric122 <karickenan97@gmail.com>
2022-01-10 15:08:59 +0100

    :lipstick: new things added landing page

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faChair, faHeart, faIdBadge } from '@fortawesome/free-solid-svg-icons'
 import './styles/Choose.css'
 
 function Choose() {
     return (
         <div className='chs2'>
         <h1 style={{color : "#404040", textAlign : "center", marginTop : "10%", fontWeight : "bold", backgroundColor : "rgb(230, 230, 230)"}}><br/><br/><span>/ </span>Why choose us?<span> /</span></h1>
+        <p style={{color : "404040", textAlign : "center", marginLeft : "10%", marginRight : "10%"}}>We are a family-owned business that has been serving the local community since 1990. We always strive for the highest quality, honest and friendly service, and do our best to help you as quickly as possible.</p>
         <div className='choose'>
+            <div className='icons'>
             <FontAwesomeIcon icon = {faHeart} size = '3x'></FontAwesomeIcon>
+            </div>
+            <div className='icons'>
+            <FontAwesomeIcon icon = {faIdBadge} size = '3x'></FontAwesomeIcon>
+            </div>
+            <div className='icons'>
+            <FontAwesomeIcon icon = {faChair} size = '3x'></FontAwesomeIcon>
+            </div>
+        </div>
+
+        <div className='choose1'>
+            <div className='icons'>
+            <FontAwesomeIcon icon = {faHeart} size = '3x'></FontAwesomeIcon>
+            </div>
+            <div className='icons'>
+            <FontAwesomeIcon icon = {faIdBadge} size = '3x'></FontAwesomeIcon>
+            </div>
+            <div className='icons'>
+            <FontAwesomeIcon icon = {faChair} size = '3x'></FontAwesomeIcon>
+        </div>
+
+
         </div>
         </div>
     )
