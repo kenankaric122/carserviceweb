bcefb72165f80f1ca05f71608d81c587f4c7d1b0
kenankaric122 <karickenan97@gmail.com>
2022-01-11 22:47:11 +0100

    :lipstick: updated about us page

---
+++ src/components/Clients.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/Clients.css';
+
+function Clients() {
+    return (
+        <div className='clients1'>
+        <h1 style={{color : "#404040", textAlign : "center", marginTop : "10%", fontWeight : "bold"}}><span>/ </span>We are always ready to assist our Clients<span> /</span></h1>
+        <div className='clients'>
+        <div className='img1'></div>
+            <div className='text1'>
+            <span style={{color : "#404040", fontWeight : "bold", fontSize : "36px"}}>We are always ready to assist our Clients<br/><br/></span>
+            Automotive’s four Frederick locations continues its 20++ plus years tradition of quality auto repair services. ehicle owners in the ederick, MD area should be well aware of the Dynamic Automotive brand of service. We strive to go above and beyond for all of our customers, who we pair services. ehicle owners.
+        </div>   
+        </div>
+        </div>
+    )
+}
+
+export default Clients
--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import AboutHome from '../components/AboutHome';
+import Clients from '../components/Clients';
 
 function Aboutus() {
     return (
         <div>
             <Navbar/>
             <AboutHome/>
+            <Clients/>
         </div>
     )
 }
