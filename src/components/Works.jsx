87b4d842941c886b5123b681aaf509c2f49fdd70
kenankaric122 <karickenan97@gmail.com>
2022-01-14 22:57:29 +0100

    :sparkles: new features

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,6 +4,8 @@ import image1 from '../assets/w1.jpg';
 import image2 from '../assets/w2.jpg';
 import image3 from '../assets/w3.jpg';
 import image4 from '../assets/w4.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarCheck, faCar, faCheckSquare, faTools } from '@fortawesome/free-solid-svg-icons';
 
 function Works() {
     return (
@@ -12,19 +14,19 @@ function Works() {
         <div className='works'>
             <div className='card2'>
                 <img src={image1}/>
-                <h2>Make an appointment</h2>
+                <h2><FontAwesomeIcon icon = {faCalendarCheck} color = '#ab0000'/>ㅤMake an appointment</h2>
             </div>
             <div className='card2'>
                 <img src={image2}/>
-                <h2>Select services</h2>
+                <h2><FontAwesomeIcon icon = {faTools} color = '#ab0000'/>ㅤSelect services</h2>
             </div>
             <div className='card2'>
                 <img src={image3}/>
-                <h2>Confirm request</h2>
+                <h2><FontAwesomeIcon icon = {faCheckSquare} color = '#ab0000'/>ㅤConfirm request</h2>
             </div>
             <div className='card2'>
                 <img src={image4}/>
-                <h2>Get your car back</h2>
+                <h2><FontAwesomeIcon icon = {faCar} color = '#ab0000'/>ㅤGet your car back</h2>
             </div>
         </div>
         </div>
