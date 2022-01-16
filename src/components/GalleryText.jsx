2e42b4397b32f4c940b65822f92f35eb41dd2a63
kenankaric122 <karickenan97@gmail.com>
2022-01-17 00:16:10 +0100

    :lipstick: Styles updated

--- src/components/GalleryText.jsx
+++ src/components/GalleryText.jsx
@@ -6,8 +6,7 @@ function GalleryText() {
         <div className='gallerytext' style={{textAlign : 'center'}}>
         <h1 style={{color : "#404040", textAlign : "center", marginTop : "10%", fontWeight : "bold"}}><span>/ </span>Here you can see our work<span> /</span></h1>
         <div className='textcontact'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-        </div>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.        </div>
         </div>
     )
 }
